503ca5c6b4ce94e0a1e8320b4fafe395c904f78b
Arun Prabhu <arun1711@gmail.com>
2025-10-16 03:06:09 -0400

    CartItems component

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -7,29 +7,48 @@ const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
+  const regex = /[^\d.-]/g;
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalAmount = 0;
+
+    cart.forEach(item => {
+        const cost = Number(item.cost.replace(regex, ''));
+
+        totalAmount += item.quantity * cost;
+    });
+
+    return totalAmount;
   };
 
   const handleContinueShopping = (e) => {
-   
+   onContinueShopping(e);
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({name: item.name, quantity: item.quantity + 1}))
   };
 
   const handleDecrement = (item) => {
-   
+    if(item.quantity > 1 ){
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1}))
+    }
+    else{
+        dispatch(removeItem(item.name))
+    }
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name));
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    return Number(item.cost.replace(regex, '')) * item.quantity
   };
 
   return (
